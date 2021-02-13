7fa3e183848cccff4f26267ca9a2206dfe0fd416
therealelyayo <60529195+therealelyayo@users.noreply.github.com>
2021-02-13 01:56:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-yum -y update
-yum -y install wget
-cd /usr/local/src
-wget http://centos-webpanel.com/cwp-el7-latest
-sh cwp-el7-latest
+#!/usr/bin/env bash
+apt update -y
+apt upgrade -y
+apt install curl nano git apt-transport-https ca-certificates gnupg2 software-properties-common -y
+curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+cd /opt
+git clone https://github.com/mailcow/mailcow-dockerized
+cd mailcow-dockerized
 exit
