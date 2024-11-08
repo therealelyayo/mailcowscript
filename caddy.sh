4c248979bc4a83ff91cba669240bb3190970ef49
therealelyayo <60529195+therealelyayo@users.noreply.github.com>
2024-11-08 23:21:03 +0100

    Create caddy.sh

---
+++ caddy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+docker run -d \
+   --name postal-caddy \
+   --restart always \
+   --network host \
+   -v /opt/postal/config/Caddyfile:/etc/caddy/Caddyfile \
+   -v /opt/postal/caddy-data:/data \
+   caddy
